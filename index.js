78f5203b60cba19a8dc7d089ed2933195beb9663
Ranjana Poojary <58767237+ranjanasalian@users.noreply.github.com>
2024-05-15 17:47:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png also fetching is done
+
+var image1 = document.querySelectorAll("img")[0]; //Finally, the selected <img> element is assigned to the variable image1, allowing you to manipulate or access properties of this element using the variable image1.
+
+image1.setAttribute("src", randomImageSource); // here the setattribute selects a random image
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2); // here the setattribute selects a random image
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  }
+  else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+
+
+
+
+
